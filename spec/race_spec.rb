72b04f238b48e91c705afde63eb81d29b1d401d4
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-01 08:31:15 -0700

    Test: create race class tests

---
+++ spec/race_spec.rb
@@ -0,0 +1,23 @@
+require './lib/candidate'
+require './lib/race'
+
+RSpec.describe Race do
+  let(:race) { Race.new('Texas Governor') }
+  it 'exists' do
+    expect(race.office).to eq('Texas Governor')
+    expect(race.candidates).to eq([])
+  end
+
+  it 'registers candidates' do
+    candidate1 = race.register_candidate!({ name: 'Diana D', party: :democrat })
+    expect(candidate1).to be_instance_of(Candidate)
+    expect(candidate1.anem).to eq('Diana D')
+    expect(candidate1.party).to eq(:democrat)
+  end
+
+  it 'returns all candidates' do
+    candidate1 = race.register_candidate!({ name: 'Diana D', party: :democrat })
+    candidate2 = race.register_candidate!({ name: 'Roberto R', party: :republican })
+    expect(race.candidates).to eq([candidate1, candidate2])
+  end
+end
