61877a40ddfc59985db3b9769a50df1adcb4da26
Kaylah Rose Mitchell <kaylahrosem@gmail.com>
2022-11-01 08:17:11 -0700

    Create Candidates

--- lib/candidate.rb
+++ lib/candidate.rb
@@ -1,2 +1,8 @@
 class Candidate
+  attr_reader :name, :party
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @party = attributes[:party]
+  end
 end
--- spec/candiate_spec.rb
+++ spec/candiate_spec.rb
@@ -1,19 +1,19 @@
 require './lib/candidate'
 
 RSpec.describe Candidate do
-    let(:diana) { Candidate.new({ name: 'Diana D', party: :democrat }) }
+  let(:diana) { Candidate.new({ name: 'Diana D', party: :democrat }) }
   it 'exists and has attributes' do
     expect(diana).to be_instance_of(Candidate)
-    expect(diana.name).to be('Diana D')
-    expect(diana.party).to be(:democrat)
+    expect(diana.name).to eq('Diana D')
+    expect(diana.party).to eq(:democrat)
   end
 
   it 'counts votes' do
-    expect(diana.votes).to be(0)
+    expect(diana.votes).to eq(0)
   end
 
   it 'votes' do
-    expect(diana.votes).to be(0)
+    expect(diana.votes).to eq(0)
     3.times diana.vote_for!
     expect
     expect(diana.votes).to eq(3)
